fe85feb875dee48d49692cfd2eb6df5a9c07ad4f
Denilson Antonio Marques Junior <denilsonjnr6@gmail.com>
2018-08-11 16:50:32 -0300

    Initial Bomb Commit
    Prototype of God's Bomb throwing power

---
+++ Assets/Scripts/God/Trap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+    public Color NormalColor;
+    public Color FlashColor;
+
+    public AnimationCurve FlashPeriod;
+    public float ExplodeTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.GetComponent<Material>().color = NormalColor;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
