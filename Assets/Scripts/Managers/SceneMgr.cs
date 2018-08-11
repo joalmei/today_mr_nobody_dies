3876d97e4f7d69d8c507bd55b40c3c2dfc94eff0
LVotto <luizvotto@gmail.com>
2018-08-11 15:05:01 -0300

    Make camera zoom over time.
    - Camera zooms over time.
    - Maximum game time is an element from SceneMgr.

--- Assets/Scripts/Managers/SceneMgr.cs
+++ Assets/Scripts/Managers/SceneMgr.cs
@@ -7,6 +7,8 @@ public class SceneMgr : MonoBehaviour
     // -------------------------------- PUBLIC ATTRIBUTES -------------------------------- //
     public float            m_globalZ = 0;
     public static float     GlobalZ { get { return m_manager.m_globalZ; } }
+    public float            m_max_time = 60;
+    public static float     MaxTime { get { return m_manager.m_max_time; } }
 
     // -------------------------------- PRIVATE ATTRIBUTES ------------------------------- //
     private static SceneMgr m_manager;
