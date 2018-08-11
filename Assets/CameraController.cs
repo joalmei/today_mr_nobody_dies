fd19b9bc667d77788c8366cb47bb388a9ed3b0b9
LVotto <luizvotto@gmail.com>
2018-08-11 13:51:16 -0300

    Add CameraController
    Made CameraController:
    - Follows Player.
    - Tries to zoom as a function of time.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform m_player;
+    public Vector3 m_offset;
+    private float m_time;
+    public float m_approach_factor;
+
+    void Start(){
+        m_time = 0;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        m_time += Time.deltaTime;
+        Debug.Log(m_time);
+        m_offset.z += m_approach_factor * m_time;
+        transform.position = m_player.position + m_offset;
+	}
+}
