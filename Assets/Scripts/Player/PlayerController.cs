e218ec6752fb2effa637cfa8a7d3ee957ec7e6c1
jvaaguiar <jv.aaguiar@gmail.com>
2018-08-11 13:22:23 -0300

    PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    // -------------------------------- PUBLIC ATTRIBUTES -------------------------------- //
+    [Header("Walk")]
+    public float            m_maxWalkSpeed              = 7;
+    public float            m_walkAcc                   = 1;
 
-	// Use this for initialization
-	void Start () {
-		
+    [Header("Jetpack")]
+    public float            m_maxJetPackSpeed = 5;
+    public float            m_jetPackAccUp              = 12;
+    public float            m_jetPackAccDown            = 5;
+    
+    // -------------------------------- PRIVATE ATTRIBUTES ------------------------------- //
+    private float           m_walkSpeed                 = 0;
+    private float           m_jetPackSpeed              = 0;
+
+    private const float     MIN_SPEED_TO_MOVE           = 0.1f;
+    private const float     GROUND_Y_VALUE_TO_DELETE    = -2.35f;
+    
+
+    // ======================================================================================
+    // PUBLIC MEMBERS
+    // ======================================================================================
+    public void Update ()
+    {
+        float   horizontal      = Input.GetAxis("Horizontal");
+        bool    enableJetPack   = Input.GetButton("Jump");
+
+        UpdateTransform(horizontal, enableJetPack);
+        UpdateGravity(enableJetPack);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // ======================================================================================
+    // PRIVATE MEMBERS
+    // ======================================================================================
+    private void UpdateTransform(float _inputHorizontal, bool _inputJetpack)
+    {
+        m_walkSpeed = Mathf.Lerp(m_walkSpeed, m_maxWalkSpeed * _inputHorizontal, Time.deltaTime * m_walkAcc);
+
+        this.transform.position += Vector3.right * Time.deltaTime * m_walkSpeed;
+
+        if (_inputJetpack)
+        {
+            m_jetPackSpeed = Mathf.Lerp(m_jetPackSpeed, m_maxJetPackSpeed, Time.deltaTime * m_jetPackAccUp);
+        }
+        else
+        {
+            if (m_jetPackSpeed > MIN_SPEED_TO_MOVE)
+            {
+                m_jetPackSpeed = Mathf.Lerp(m_jetPackSpeed, 0, Time.deltaTime * m_jetPackAccDown);
+            }
+            else
+            {
+                m_jetPackSpeed = 0;
+            }
+        }
+
+        if (m_jetPackSpeed > 0)
+        {
+            this.transform.position += Vector3.up * Time.deltaTime * m_jetPackSpeed;
+        }
+    }
+
+    // ======================================================================================
+    private void UpdateGravity (bool _enableJetPack)
+    {
+        //if (!_enableJetPack)
+        {
+            this.transform.position += Time.deltaTime * Physics.gravity * .7f;
+
+            if (this.transform.position.y < GROUND_Y_VALUE_TO_DELETE)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, GROUND_Y_VALUE_TO_DELETE, this.transform.position.z);
+            }
+        }
+    }
 }
