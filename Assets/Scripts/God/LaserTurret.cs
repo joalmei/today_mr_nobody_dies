a6f0714c0cdef1a7c4782b1897f9b64e53865b89
Denilson Antonio Marques Junior <denilsonjnr6@gmail.com>
2018-08-12 15:34:18 -0300

    Made Laser Turrets have a lifespan

--- Assets/Scripts/God/LaserTurret.cs
+++ Assets/Scripts/God/LaserTurret.cs
@@ -8,9 +8,12 @@ public class LaserTurret : MonoBehaviour
     public float Angle;
     public bool Active = false;
 
+    public float Lifespan;
+
     public GameObject PrefabLaser;
 
     private float LastFire = 0.0f;
+    private float SpawnedTime = 0.0f;
 
     public void Activate(float AngleParam)
     {
@@ -18,6 +21,8 @@ public class LaserTurret : MonoBehaviour
         Active = true;
         Angle = AngleParam;
         LastFire = Time.time;
+
+        SpawnedTime = Time.time;
     }
 
     // Update is called once per frame
@@ -25,7 +30,10 @@ public class LaserTurret : MonoBehaviour
     {
         if (Active)
         {
-            if (Time.time > LastFire + FirePeriod)
+            if (Time.time > SpawnedTime + Lifespan)
+            {
+                Destroy(gameObject);
+            } else if (Time.time > LastFire + FirePeriod)
             {
                 GameObject Laser = Instantiate(
                     PrefabLaser,
@@ -34,7 +42,7 @@ public class LaserTurret : MonoBehaviour
                 ) as GameObject;
 
                 Laser.GetComponent<Laser>().Activate();
-                
+
                 LastFire = Time.time;
             }
         }
